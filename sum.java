14acf8173f87617011c3b03e05c5d1165e65923d
Sharan161 <sharan6787@gmail.com>
2025-07-05 20:44:56 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -4,7 +4,7 @@ public class sum {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int sum=0,a;
-        while(n>0){
+        for(;n>0;){
          a=n%10;
             sum+=a;
             n=n/10;
